c0107300e13a18e1f7b4a4549fe39f80b046ab6a
CMonkey <42.codemonkey@gmail.com>
2018-12-29 14:14:24 +0800

    add match guess in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
     println!("Guess the number!");
 
@@ -14,6 +16,16 @@ fn main() {
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number!");
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number){
+        Ordering::Less => println!("Too smaill"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal => println!("You win!"),
+
+    }
 
 }
