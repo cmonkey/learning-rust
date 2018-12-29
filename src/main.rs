8119cf4991474c8ca0c53f89f52fc007ea69d062
CMonkey <42.codemonkey@gmail.com>
2018-12-29 14:45:29 +0800

    add array to main

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,13 @@ fn main() {
 
     println!("tuple = {}, {}, {}", tup.0, tup.1, tup.2);
 
+    let array = [1,2,3,4,5,6];
+
+    println!("array [0] = {}", array[0]);
+
+    let array:[i32; 5] = [1,2,3,4,5];
+
+    println!("array [4] = {}", array[4]);
 
     println!("Guess the number!");
 
